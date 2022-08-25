20e7fcb6bbdf43d87669428100804ed6ed1d382c
SPTAU <liu1030836518@163.com>
2022-08-25 11:10:43 +0800

    添加三维曲面模型实验

---
+++ experimentation2_1.py
@@ -0,0 +1,60 @@
+"""
+Filename: experimentation2_1.py
+Author: SPTAU
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+x_data = [1.0, 2.0, 3.0]
+y_data = [2.0, 4.0, 6.0]
+w = 0
+b = 0
+
+
+def forward(x):
+    """forward
+
+    Returns: x * w + b
+    """
+
+    return x * w + b
+
+
+def loss(x, y):
+    """loss
+
+    Returns: (y_pred - y) * (y_pred - y)
+    """
+    y_pred = forward(x)
+    return (y_pred - y) * (y_pred - y)
+
+
+w_list = []
+mse_list = []
+MSE = []
+for w in np.arange(-4, 5, 1):
+    mse_list = []
+    b_list = []
+    for b in np.arange(-4, 5, 1):
+        print("w=", w)
+        print("b=", b)
+        loss_sum = 0
+        for x_val, y_val in zip(x_data, y_data):
+            y_pred_val = forward(x_val)
+            loss_val = loss(x_val, y_val)
+            loss_sum += loss_val
+            print("\t", x_val, y_val, y_pred_val, loss_val)
+        mse = loss_sum / len(x_data)
+        print("MSE=", mse)
+        b_list.append(b)
+        mse_list.append(mse)
+    w_list.append(w)
+    MSE.append(mse_list)
+
+MSE = np.array(MSE)
+fig = plt.figure()
+ax = fig.gca(projection="3d")
+W, B = np.meshgrid(w_list, b_list)
+ax.plot_surface(W, B, MSE)
+plt.show()
